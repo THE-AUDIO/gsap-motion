76f21aa5da0457524b6fa7e878c253d0b69e8808
THE-AUDIO <nandraina.dev22@gmail.com>
2025-04-25 23:20:13 +0300

    gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'gsap-motion';
+  @ViewChild('element1') element0!: ElementRef;
+
+  imageUrl = [
+    {nom:"sports-car"},
+    {nom:"modern_speaker"},
+    {nom:"Red-sunset"},
+    {nom:"sports-car"},
+    {nom:"modern_speaker"},
+    {nom:"Red-sunset"}
+  ]
+
+
+  constructor(
+     @Inject(PLATFORM_ID) private platformId: Object,
+  ){}
+  cardAnim(classe: string){
+    gsap.to(classe,{
+      duration:.5,
+      rotateX:"15deg",
+      ease: 'back',
+      scrollTrigger:{
+        trigger:classe,
+        scrub:1 ,
+        start: "top 10%",
+        end: "bottom 30%",
+      }
+    })
+  }
+  lunchAnim(){
+    for (let i = 0; i < this.imageUrl.length; i++) {
+        const classe = `.image${i}`
+        console.log(classe);
+        
+        this.cardAnim(classe)
+    }
+  }
+  ngAfterViewInit(): void {
+      if(isPlatformBrowser(this.platformId)){
+        this.lunchAnim()
+      }
+  }
 }
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary":"#313c45",
+        "secondary":"#00e3b8",
+        "over":"#f5fafd"
+      }
+    },
+  },
+  plugins: [],
+}
