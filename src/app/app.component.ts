a4450e912d698bdb2f5fed7610d6ca93a508bd94
THE-AUDIO <nandraina.dev22@gmail.com>
2025-04-26 11:57:56 +0300

    change name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Inject, NgZone, OnInit, PLATFORM_ID, QueryList, Renderer2, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
 import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import TextPlugin from 'gsap/TextPlugin';
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,47 +14,61 @@ gsap.registerPlugin(ScrollTrigger)
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit , OnInit{
   title = 'gsap-motion';
-  @ViewChild('element1') element0!: ElementRef;
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private ngZone: NgZone
+
+ ){}
+  ngOnInit(): void {
+  }
+  nom: String = "sports-car ";
+  color: String = "#00e3b8";
+  @ViewChild('texte') texte!: ElementRef;
 
   imageUrl = [
-    {nom:"sports-car"},
-    {nom:"modern_speaker"},
-    {nom:"Red-sunset"},
-    {nom:"sports-car"},
-    {nom:"modern_speaker"},
-    {nom:"Red-sunset"}
+    {nom:"sports-car", color:"#00e3b8"},
+    {nom:"speaker", color:"#f5fafd"},
+    {nom:"Red-sunset", color:"#00e3b8"},
+    {nom:"sports-car", color:"#f5fafd"},
+    {nom:"speaker", color:"#00e3b8"},
+    {nom:"Red-sunset", color:"#f5fafd"}
   ]
-
-
-  constructor(
-     @Inject(PLATFORM_ID) private platformId: Object,
-  ){}
-  cardAnim(classe: string){
+  
+  cardAnim(classe: string, title:string, color:string){
     gsap.to(classe,{
-      duration:.5,
-      rotateX:"15deg",
+      duration:.25,
+      rotateX:"10deg",
       ease: 'back',
       scrollTrigger:{
         trigger:classe,
         scrub:1 ,
-        start: "top 10%",
+        start: "top 15%",
         end: "bottom 30%",
-      }
+      },
+      onUpdate:()=>{
+        this.ngZone.run(() => {
+          this.nom = title;
+          this.color = color;
+        });
+      } 
     })
   }
   lunchAnim(){
-    for (let i = 0; i < this.imageUrl.length; i++) {
-        const classe = `.image${i}`
-        console.log(classe);
-        
-        this.cardAnim(classe)
-    }
+    this.imageUrl.forEach((elt)=>{
+      const classe = `.image${this.imageUrl.indexOf(elt)}`
+      this.cardAnim(classe, elt.nom, elt.color)
+
+    })
+  }
+  teste(){
+    console.log(true);
+    
   }
   ngAfterViewInit(): void {
       if(isPlatformBrowser(this.platformId)){
-        this.lunchAnim()
+        this.lunchAnim();
       }
   }
 }
